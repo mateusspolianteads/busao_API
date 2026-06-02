e119daa90d913ddf7a08286b4b6ca3fd44118f6f
matspol <mateus.spoliantads@gmail.com>
2026-06-02 15:25:57 -0300

    Teste de log no main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from database import Base, engine
+from datetime import datetime
+
+print("Início:", datetime.now())
 from routes import (
     usuarios,
     clientes,
@@ -9,8 +12,11 @@ from routes import (
     upload,
     importacao_route,
     login_route,
-    export_route
+    export_route,
 )
+
+print("Rotas carregadas:", datetime.now())
+
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -20,23 +26,17 @@ from models.evento import Evento
 from models.pedido import Pedido
 from models.categoria import Categoria
 
-app = FastAPI(
-    title="Busão do Rolê API",
-    version="1.0.0"
-)
+app = FastAPI(title="Busão do Rolê API", version="1.0.0")
 
 # CORS
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "https://busaodorole.vercel.app",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
     ],
-
     allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
-
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -44,6 +44,8 @@ app.add_middleware(
 
 # Cria tabelas
 Base.metadata.create_all(bind=engine)
+
+print("Tabelas verificadas:", datetime.now())
 
 # Rotas
 app.include_router(usuarios.router)
@@ -56,20 +58,15 @@ app.include_router(importacao_route.router)
 app.include_router(login_route.router)
 app.include_router(export_route.router)
 
+
 # Home
 @app.get("/")
 def home():
-    return {
-        "mensagem": "API Busão do Rolê funcionando"
-    }
+    return {"mensagem": "API Busão do Rolê funcionando"}
+
 
 # Rodar API
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
