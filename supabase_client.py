178678a8d174405dd614f6c1edc34a5758f83ac7
matspol <mateus.spoliantads@gmail.com>
2026-06-03 09:49:19 -0300

    Ajustes Deploy

--- supabase_client.py
+++ supabase_client.py
@@ -2,25 +2,23 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client
 
-"""
-OBSERVAÇÃO IMPORTANTE:
-- Use Python 3.12 (versões diferentes podem causar erro no supabase)
-- Recrie o venv quando trocar de versão do Python:
+#OBSERVAÇÃO IMPORTANTE:
+#- Use Python 3.12 (versões diferentes podem causar erro no supabase)
+#- Recrie o venv quando trocar de versão do Python:
 
-INSTALAR O PYTHON 3.12 -> winget install Python.Python.3.12
+# INSTALAR O PYTHON 3.12 -> winget install Python.Python.3.12
 
-  1. deletar venv atual
-  2. py -3.12 -m venv venv
-  3. venv\Scripts\activate
-  4. pip install -r requirements.txt
+#  1. deletar venv atual
+#  2. py -3.12 -m venv venv
+#  3. venv\\Scripts\\activate
+#  4. pip install -r requirements.txt
 
-- Criar arquivo .env na raiz:
-  DATABASE_URL=... (se você já tem o .env provavelmetne já tem esse, então é só adicionar os dois de baixo)
-  SUPABASE_URL=...
-  SUPABASE_KEY=...
+# - Criar arquivo .env na raiz:
+#  DATABASE_URL=... (se você já tem o .env provavelmetne já tem esse, então é só adicionar os dois de baixo)
+#  SUPABASE_URL=...
+#  SUPABASE_KEY=...
 
-- Reiniciar o VS Code/terminal após mudar o ambiente
-"""
+# - Reiniciar o VS Code/terminal após mudar o ambiente
 
 load_dotenv()
 
